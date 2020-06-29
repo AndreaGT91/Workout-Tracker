69bad4b45b024bdcf225bdb1d96412252bed7440
Andrea Bentley <andrea@bentleytech.com>
2020-06-28 20:28:15 -0400

    Enabled html routes

--- server.js
+++ server.js
@@ -10,14 +10,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+require("./routes/html-routes.js")(app);
+// require("./routes/api-routes.js")(app);
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
-
-// routes
-app.use(require("./routes/html-routes.js"));
-app.use(require("./routes/api-routes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
